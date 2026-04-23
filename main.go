bb814a8d6bf5f88eb6174e4906912e73ef4130c8
BOUSSENNA Mohamed_Amine <mohamed_amine.boussenna@bordeaux-inp.fr>
2026-04-23 23:04:13 +0200

    feat(ssh): SSH handshake with password auth logging

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/Am1ne-bou/ssh-honeypot/config"
+	"github.com/Am1ne-bou/ssh-honeypot/hostkey"
 	"github.com/Am1ne-bou/ssh-honeypot/logger"
 	"github.com/Am1ne-bou/ssh-honeypot/server"
 )
@@ -12,7 +13,19 @@ func main() {
 	cfg := config.Parse()
 	log := logger.New(os.Stderr)
 
-	if err := server.Serve(cfg.Addr, log); err != nil {
+	signer, err := hostkey.Generate()
+	if err != nil {
+		log.Error("host key generation failed", "err", err)
+		os.Exit(1)
+	}
+
+	opts := &server.Options{
+		Addr:   cfg.Addr,
+		Signer: signer,
+		Logger: log,
+	}
+
+	if err := server.Serve(opts); err != nil {
 		log.Error("server failed", "err", err)
 		os.Exit(1)
 	}
